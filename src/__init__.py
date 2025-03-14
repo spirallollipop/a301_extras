88f28e4e16b9a86f3cfa1f87967331c070f19bc3
Phil Austin <paustin@eos.ubc.ca>
2025-03-14 08:57:15 -0700

    update

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("a301_thermo")
+    __version__ = version("plot_utils")
 except PackageNotFoundError:
     __version__ = "unknown version"
 
@@ -12,4 +12,4 @@ try:
 except ImportError:
     version_tuple = (0, 0, "unknown version")
 
-print("in a301_thermo init")
+print("in a301_extras init")
---
+++ test_imports.py
@@ -0,0 +1,7 @@
+import plot_utils
+import thermo_utils
+
+print(f"{dir(plot_utils)=}")
+print(f"{dir(thermo_utils)=}")
+
+
